49fb7b5bbec24653eb1876f4e60017ad54ee82e7
Douglas Sanchez <sanchezdouglas366@gmail.com>
2022-11-22 00:49:35 +0100

    Act 1

---
+++ src/MoviesGrid.jsx
@@ -0,0 +1,11 @@
+import movies from "./movies.json";
+
+export function MoviesGrid() {
+    return (
+    <ul>
+        {movies.map(function (movie) {
+           return <li>{movie.title}</li>
+        })}
+    </ul>
+    );
+}
